872d45dbfadc69ad87be863158ebd3fae851fc8a
emmawirts <emmaroselee14@gmail.com>
2025-02-10 14:47:56 -0500

    replace console.log with jokeEl

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function generateJoke() {
    fetch('https://icanhazdadjoke.com', config)
      .then((res) => res.json())
      .then((data) => {
-       console.log(data);
+        jokeEl.innerHTML = data.joke;
      });
  }
 
